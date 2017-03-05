910e58478c8af5a7f3739538ee34c20b1c2d54fa
adeturner <adrian.turner@maersk.com>
2017-03-05 09:23:27 +0000

    ...

--- ogg4bd-build.sh
+++ ogg4bd-build.sh
@@ -2,7 +2,7 @@
 #########################################################################################
 ## OGG for Big Data
 #########################################################################################
-# Installs Oracle Goldengate for Big Data 12.3.1 on an existing Oracle database 
+# Installs Oracle Goldengate for Big Data 12.2.0.1.4 on an existing Oracle database 
 # built via https://github.com/mgis-architects/terraform/tree/master/azure/oracledb
 # This script only supports Azure currently, mainly due to the disk persistence method
 #
@@ -85,19 +85,216 @@ function installRPMs()
     fi
 }
 
+function fixSwap()
+{
+    cat /etc/waagent.conf | while read LINE
+    do
+        if [ "$LINE" == "ResourceDisk.EnableSwap=n" ]; then
+                LINE="ResourceDisk.EnableSwap=y"
+        fi
+
+        if [ "$LINE" == "ResourceDisk.SwapSizeMB=2048" ]; then
+                LINE="ResourceDisk.SwapSizeMB=14000"
+        fi
+        echo $LINE
+    done > /tmp/waagent.conf
+    /bin/cp /tmp/waagent.conf /etc/waagent.conf
+    systemctl restart waagent.service
+}
+
+function addGroups()
+{
+    local l_group_list=$LOG_DIR/groups.${g_prog}.lst.$$
+    local l_found
+    local l_user
+    local l_x
+    local l_gid
+    local l_newgid
+    local l_newgname
+
+    cat > $l_group_list << EOFaddGroups
+54321 oinstall
+EOFaddGroups
+
+    while read l_newgid l_newgname
+    do    
+    
+        local l_found=0
+        grep $l_newgid /etc/group | while IFS=: read l_gname l_x l_gid
+        do
+            if [ $l_gname != $l_newgname ]; then
+                fatalError "addGroups(): gid $l_newgid exists with a different group name $l_gname"
+            fi
+        done
+        
+        log "addGroups(): /usr/sbin/groupadd -g $l_newgid $l_newgname"
+        if ! /usr/sbin/groupadd -g $l_newgid $l_newgname; then 
+            log "addGroups() failed on $l_newgid $l_newgname"
+        fi
+    
+    done < $l_group_list   
+}
+
+function addUsers()
+{
+
+    if ! id -a oracle 2> /dev/null; then
+        /usr/sbin/useradd -u 54321 -g oinstall oracle -p c6kTxMi2LR1l2
+    else
+        fatalError "addUsers(): user 54321/oracle already exists"
+    fi
+}
+
+function addLimits()
+{
+    local l_mem
+
+    cp /etc/security/limits.conf /etc/security/limits.conf.preDB
+    
+    # at least 90 percent of the current RAM
+    let l_mem=`grep MemTotal /proc/meminfo | awk '{print $2}'`*91/100
+
+    cat >> /etc/security/limits.conf << EOFaddLimits
+oracle           soft    nproc     16384
+oracle           hard    nproc     16384
+oracle           soft    nofile    65536
+oracle           hard    nofile    65536
+oracle           soft    stack     10240
+oracle           hard    stack     10240
+oracle           soft    memlock  $l_mem
+oracle           hard    memlock  $l_mem
+EOFaddLimits
+}
+
+function addPam()
+{
+
+    cp /etc/pam.d/login /etc/pam.d/login.pre.$$
+    
+    cat >> /etc/pam.d/login << EOFaddPam
+# Doc ID 1529864.1
+session    required     pam_limits.so
+EOFaddPam
+
+}
+
+function makeFolders()
+{
+    local l_error=0
+    if ! mkdir -p /u01/app/oracle; then l_error=1; fi
+    if ! chown oracle:oinstall /u01/app/oracle; then l_error=1; fi
+    if ! chmod -R 775 /u01/; then l_error=1; fi
+    if [ $l_error -eq 1 ]; then
+        fatalError "makeFolders(): error creating folders"
+    fi
+}
 
 function oracleProfile() 
 {
-    cat >> /home/oracle/.bash_profile << EOForacleProfile
+    cat >> /home/oracle/.bashrc << EOForacleProfile1
+    if [ -t 0 ]; then
+       stty intr ^C
+    fi
+EOForacleProfile1
+
+    cat >> /home/oracle/.bash_profile << EOForacleProfile2
+    umask 022
+    set -o vi
+    export EDITOR=vi
+    export TMP=/tmp
+    export TMPDIR=/tmp
+    export OGG_HOME=${oggHome}
     export JAVA_HOME=/usr/lib/jvm
     export LD_LIBRARY_PATH=\$JAVA_HOME/jre/lib/amd64/server:$LD_LIBRARY_PATH
-    #export PATH=$PATH:/u01/app/oracle/product/12.3.0/ogg4bd
-EOForacleProfile
+    export PATH=$PATH:${ogg4bdHome}
+EOForacleProfile2
+
+    chown oracle:oinstall /home/oracle/.bashrc 
+    chown oracle:oinstall /home/oracle/.bash_profile
+}
+
+createFilesystem()
+{
+    # createFilesystem /u01 $l_disk $diskSectors  
+    # size is diskSectors-128 (offset)
+
+    local p_filesystem=$1
+    local p_disk=$2
+    local p_sizeInSectors=$3
+    local l_sectors
+    local l_layoutFile=$LOG_DIR/sfdisk.${g_prog}_install.log.$$
+    
+    if [ -z $p_filesystem ] || [ -z $p_disk ] || [ -z $p_sizeInSectors ]; then
+        fatalError "createFilesystem(): Expected usage mount,device,numsectors, got $p_filesystem,$p_disk,$p_sizeInSectors"
+    fi
+    
+    let l_sectors=$p_sizeInSectors-128
+    
+    cat > $l_layoutFile << EOFsdcLayout
+# partition table of /dev/sdc
+unit: sectors
+
+/dev/sdc1 : start=     128, size=  ${l_sectors}, Id= 83
+/dev/sdc2 : start=        0, size=        0, Id= 0
+/dev/sdc3 : start=        0, size=        0, Id= 0
+/dev/sdc4 : start=        0, size=        0, Id= 0
+EOFsdcLayout
+
+    set -x # debug has been useful here
+
+    if ! sfdisk $p_disk < $l_layoutFile; then fatalError "createFilesystem(): $p_disk does not exist"; fi
+    
+    sleep 4 # add a delay - experiencing occasional "cannot stat" for mkfs
+    
+    log "createFilesystem(): Dump partition table for $p_disk"
+    fdisk -l 
+    
+    if ! mkfs.ext4 ${p_disk}1; then fatalError "createFilesystem(): mkfs.ext4 ${p_disk}1"; fi
+    
+    if ! mkdir -p $p_filesystem; then fatalError "createFilesystem(): mkdir $p_filesystem failed"; fi
+    
+    if ! chmod 755 $p_filesystem; then fatalError "createFilesystem(): chmod $p_filesystem failed"; fi
+    
+    if ! chown oracle:oinstall $p_filesystem; then fatalError "createFilesystem(): chown $p_filesystem failed"; fi
+    
+    if ! mount ${p_disk}1 $p_filesystem; then fatalError "createFilesystem(): mount $p_disk $p_filesytem failed"; fi
+
+    log "createFilesystem(): Dump blkid"
+    blkid
+    
+    if ! blkid | egrep ${p_disk}1 | awk '{printf "%s\t'${p_filesystem}' \t ext4 \t defaults \t 1 \t2\n", $2}' >> /etc/fstab; then fatalError "createFilesystem(): fstab update failed"; fi
+
+    log "createFilesystem() fstab success: $(grep $p_disk /etc/fstab)"
+
+    set +x    
+}
+
+function allocateStorage() 
+{
+    local l_disk
+    local l_size
+    local l_sectors
+    local l_hasPartition
+
+    for l_disk in /dev/sd? 
+    do
+         l_hasPartition=$(( $(fdisk -l $l_disk | wc -l) != 6 ? 1 : 0 ))
+        # only use if it doesnt already have a blkid or udev UUID
+        if [ $l_hasPartition -eq 0 ]; then
+            let l_size=`fdisk -l $l_disk | grep 'Disk.*sectors' | awk '{print $5}'`/1024/1024/1024
+            let l_sectors=`fdisk -l $l_disk | grep 'Disk.*sectors' | awk '{print $7}'`
+            
+            if [ $u01_Disk_Size_In_GB -eq $l_size ]; then
+                log "allocateStorage(): Creating /u01 on $l_disk"
+                createFilesystem /u01 $l_disk $l_sectors
+            fi
+        fi
+    done   
 }
 
 function mountMedia() {
 
-    if [ -f /mnt/software/ogg4bd12301/V839824-01.zip ]; then
+    if [ -f /mnt/software/ogg4bd12201/p24816159_122014_Linux-x86-64.zip ]; then
     
         log "mountMedia(): Filesystem already mounted"
         
@@ -146,14 +343,11 @@ EOF2
 
 installOgg4bd()
 {
-    local l_installdir=/u01/app/oracle/product/12.3.0/ogg4bd
-    local l_media=/mnt/software/ogg4bd12301/V839824-01.zip
+    local l_installdir=${ogg4bdHome}
+    local l_media=${ogg4bdMedia}
     local l_tmp_script=$LOG_DIR/$g_prog.installOgg4bd.$$.sh
     local l_log=$LOG_DIR/$g_prog.ogg4bdTestSuite.$$.installOgg4bd.log
     
-    cat > $l_tmp_script << EOFckp
-
-
     if [ ! -f ${l_media} ]; then
         fatalError "installGridHome(): media missing ${l_media}"
     fi
@@ -170,21 +364,21 @@ installOgg4bd()
     
     rm -f ggs_Adapters_Linux_x64.tar
     
-    ./ggsci  << EOFggsci1
+    ./ggsci  << EOFggscia
        CREATE SUBDIRS 
-EOFggsci1
+EOFggscia
 
     echo "PORT ${ogg4bdMgrPort}" > ${l_installdir}/dirprm/mgr.prm
     
-    ./ggsci  << EOFggsci2
+    ./ggsci  << EOFggscib
         START MGR 
-EOFggsci2
+EOFggscib
 
     sleep 3
     
-    ./ggsci  << EOFggsci2
+    ./ggsci  << EOFggscic
         INFO MGR 
-EOFggsci2
+EOFggscic
 
 EOFogg4bd
 
@@ -192,7 +386,7 @@ EOFogg4bd
 
 }
 
-function() installKakfaConnect
+function installKakfaConnect()
 {
     local l_tmp_script=$LOG_DIR/$g_prog.installKafkaConnect.$$.sh
     local l_log=$LOG_DIR/$g_prog.installKafkaConnect.$$.log
@@ -205,7 +399,42 @@ function() installKakfaConnect
     tar xf /tmp/OGG_KafkaConnectHandlerFormatter1.0.tar
 EOFkch
 
+    cp /u01/kafka-connect/bin/ogg-kafka-connect-1.0.jar ${ogg4bdHome}/ggjava/resources/lib
+    
     su - oracle -c "bash -x $l_tmp_script" |tee ${l_log}
+}
+
+function installConfluent() 
+{
+    confluentVersion=3.0
+    # confluentVersion=3.1
+
+    # http://docs.confluent.io/3.0.1/installation.html
+    # http://docs.confluent.io/3.1.2/installation.html
+    # lot of effort to get a kafka client... 
+    # will be installed here... /usr/share/java/kafka
+    # sudo yum -y remove confluent-platform-2.11
+
+    sudo rpm --import http://packages.confluent.io/rpm/${confluentVersion}/archive.key
+    sudo su - -c "cat > /etc/yum.repos.d/confluent.repo << EOFrepo    
+[Confluent.dist]
+name=Confluent repository (dist)
+baseurl=http://packages.confluent.io/rpm/${confluentVersion}/7
+gpgcheck=1
+gpgkey=http://packages.confluent.io/rpm/${confluentVersion}/archive.key
+enabled=1
+
+[Confluent]
+name=Confluent repository
+baseurl=http://packages.confluent.io/rpm/${confluentVersion}
+gpgcheck=1
+gpgkey=http://packages.confluent.io/rpm/${confluentVersion}/archive.key
+enabled=1
+
+EOFrepo
+"
+    sudo yum clean all
+    sudo yum -y install confluent-platform-2.11
 }
 
 function run()
@@ -218,21 +447,43 @@ function run()
     fi
 
     eval `grep ogg4bdMgrPort $INI_FILE`
+    eval `grep ogg4bdHome $INI_FILE`
+    eval `grep u01_Disk_Size_In_GB $INI_FILE`
+    eval `grep ogg4bdMedia $INI_FILE`
 
     l_str=""
     if [ -z $ogg4bdMgrPort ]; then
         l_str+="ogg4bdMgrPort not found in $INI_FILE; "
     fi
+    if [ -z $ogg4bdHome ]; then
+        l_str+="ogg4bdHome not found in $INI_FILE; "
+    fi
+    l_str=""
+    if [ -z $u01_Disk_Size_In_GB ]; then
+        l_str+="asmStorage(): u01_Disk_Size_In_GB not found in $INI_FILE; "
+    fi
+    if [ -z $ogg4bdMedia ]; then
+        l_str+="asmStorage(): ogg4bdMedia not found in $INI_FILE; "
+    fi
     if ! [ -z $l_str ]; then
-        fatalError "installSimpleSchema(): $l_str"
+        fatalError "$g_prog(): $l_str"
     fi
     
     # function calls
-    installRPMs
+    fixSwap
+    installRPMs 
+    addGroups
+    addUsers
+    addLimits
+    addPam
+    allocateStorage
+    makeFolders
     oracleProfile
     mountMedia
+    oracleProfile
     installOgg4bd
     installKakfaConnect
+    installConfluent
 }
 
 
--- ogg4bd-testsuite.sh
+++ ogg4bd-testsuite.sh
@@ -45,11 +45,6 @@ THISDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 SCR=$(basename "${BASH_SOURCE[0]}")
 THIS_SCRIPT=$THISDIR/$SCR
 
-oggHome=/u01/app/oracle/product/12.2.1/ogg
-ogg4bdHome=/u01/app/oracle/product/12.3.0/ogg4bd
-
-
-
 ######################################################
 ## log()
 ##
@@ -82,176 +77,8 @@ function fatalError ()
 }
 
 ##################################################################################
-# Leaving in here as may be useful
-##################################################################################
-# function installDocker()
-# {
-#     sudo yum -y install yum-utils
-#     sudo yum-config-manager --add-repo https://docs.docker.com/engine/installation/linux/repo_files/centos/docker.repo
-#     sudo yum makecache fast
-#     sudo yum list docker-engine.x86_64  --showduplicates |sort -r
-#     sudo yum -y install docker-engine-selinux-1.12.6-1.el7.centos
-#     sudo yum -y install docker-engine-1.12.6-1.el7.centos
-#     sudo systemctl start docker
-#     sudo systemctl status docker
-# }
-#
-# function createTopics() {
-#     sudo docker run --net=host --rm confluentinc/cp-kafka:3.1.2 kafka-topics --create --topic sstopic --partitions 3 --replication-factor 3 --if-not-exists --zookeeper 10.135.50.4:22181
-#     sudo docker run --net=host --rm confluentinc/cp-kafka:3.1.2 kafka-topics --describe --topic sstopic --zookeeper 10.135.50.4:22181
-# }
-# 
-# function customKafkaProducer() {
-# 
-#     local l_tmp_script=$LOG_DIR/$g_prog.ogg4bdTestSuite.$$.customKafkaProducer.sh
-#     local l_log=$LOG_DIR/$g_prog.ogg4bdTestSuite.$$.customKafkaProducer.log
-#     
-#     cat > $l_tmp_script << EOFckp
-# 
-# cd /u01/app/oracle/product/12.3.0/ogg4bd/dirprm
-# 
-# cat > custom_kafka_producer.properties << EOFckp1
-# bootstrap.servers=${bootstrapServers}
-# acks=1
-# compression.type=gzip
-# reconnect.backoff.ms=1000
-# value.serializer=org.apache.kafka.common.serialization.ByteArraySerializer
-# key.serializer=org.apache.kafka.common.serialization.ByteArraySerializer
-# # 100KB per partition
-# batch.size=102400
-# linger.ms=10000
-# EOFckp1
-# 
-# EOFckp
-# 
-#     su - oracle -c "bash -x $l_tmp_script" |tee ${l_log}
-# 
-# }
-# 
-# function kafkaProps() {
-# 
-#     local l_tmp_script=$LOG_DIR/$g_prog.ogg4bdTestSuite.$$.kafkaProps.sh
-#     local l_log=$LOG_DIR/$g_prog.ogg4bdTestSuite.$$.kafkaProps.log
-#     
-#     cat > $l_tmp_script << EOFkp
-# 
-#     cd /u01/app/oracle/product/12.3.0/ogg4bd/dirprm
-#     
-#     cat > kafka.props << EOFkp1
-# 
-# gg.handlerlist = kafkahandler
-# gg.handler.kafkahandler.type=kafka
-# gg.handler.kafkahandler.KafkaProducerConfigFile=custom_kafka_producer.properties
-# gg.handler.kafkahandler.TopicName=${topic1}
-# gg.handler.kafkahandler.format=avro_op
-# gg.handler.kafkahandler.SchemaTopicName=${schemaTopic1}
-# gg.handler.kafkahandler.BlockingSend=false
-# gg.handler.kafkahandler.includeTokens=false
-# gg.handler.kafkahandler.mode=tx
-# #gg.handler.kafkahandler.maxGroupSize=100, 1Mb
-# #gg.handler.kafkahandler.minGroupSize=50, 500Kb
-# goldengate.userexit.timestamp=utc
-# goldengate.userexit.writers=javawriter
-# javawriter.stats.display=TRUE
-# javawriter.stats.full=TRUE
-# gg.log=log4j
-# gg.log.level=INFO
-# gg.report.time=30sec
-# gg.classpath=dirprm/:/usr/share/java/kafka/*
-# #Sample gg.classpath for HDP
-# #gg.classpath=/etc/kafka/conf:/usr/hdp/current/kafka-broker/libs/*
-# javawriter.bootoptions=-Xmx512m -Xms32m -Djava.class.path=ggjava/ggjava.jar
-# EOFkp1
-# 
-# EOFkp
-# 
-#     su - oracle -c "bash -x $l_tmp_script" |tee ${l_log}
-# }
-# 
-# function irkafka() {
-# 
-#     local l_tmp_script=$LOG_DIR/$g_prog.ogg4bdTestSuite.$$.irkafka.sh
-#     local l_log=$LOG_DIR/$g_prog.ogg4bdTestSuite.$$.irkafka.log
-#     
-#     cat > $l_tmp_script << EOFrk
-# 
-#     cd /u01/app/oracle/product/12.3.0/ogg4bd/dirprm
-#     
-#     cat > irkafka.prm << EOFrk1
-# SPECIALRUN
-# END RUNTIME
-# EXTFILE /u01/oggbd/dirdat/initld
-# --
-# TARGETDB LIBFILE libggjava.so SET property=dirprm/kafka.props
-# REPORTCOUNT EVERY 1 MINUTES, RATE
-# GROUPTRANSOPS 10000
-# MAP ade.*, TARGET bd.*;
-# EOFrk1
-# 
-# cd /u01/app/oracle/product/12.3.0/ogg4bd
-# ./replicat paramfile dirprm/irkafka.prm reportfile dirrpt/irkafka.rpt
-#     
-# EOFrk
-# 
-#     su - oracle -c "bash -x $l_tmp_script" |tee ${l_log}
-# }
-# 
-# function rkafka() {
-# 
-#     local l_tmp_script=$LOG_DIR/$g_prog.ogg4bdTestSuite.$$.rkafka.sh
-#     local l_log=$LOG_DIR/$g_prog.ogg4bdTestSuite.$$.rkafka.log
-#     
-#     cat > $l_tmp_script << EOFrk
-# 
-#     cat > rkafka.prm << EOFrk1
-# REPLICAT rkafka
-# -- Command to add REPLICAT
-# -- add replicat rkafka, exttrail AdapterExamples/trail/tr
-# TARGETDB LIBFILE libggjava.so SET property=dirprm/kafka.props
-# REPORTCOUNT EVERY 1 MINUTES, RATE
-# GROUPTRANSOPS 10000
-# MAP ade.*, TARGET bd.*;
-# EOFrk1
-#     
-# EOFrk
-# 
-#     su - oracle -c "bash -x $l_tmp_script" |tee ${l_log}
-# }
-
-
-##################################################################################
 # Confluent setup
 ##################################################################################
-
-function installConfluent() 
-{
-
-    # http://docs.confluent.io/3.1.2/installation.html
-    # lot of effort to get a kafka client... 
-    # will be installed here... /usr/share/java/kafka
-
-    sudo rpm --import http://packages.confluent.io/rpm/3.1/archive.key
-    sudo su - -c "cat > /etc/yum.repos.d/confluent.repo << EOFrepo    
-[Confluent.dist]
-name=Confluent repository (dist)
-baseurl=http://packages.confluent.io/rpm/3.1/7
-gpgcheck=1
-gpgkey=http://packages.confluent.io/rpm/3.1/archive.key
-enabled=1
-
-[Confluent]
-name=Confluent repository
-baseurl=http://packages.confluent.io/rpm/3.1
-gpgcheck=1
-gpgkey=http://packages.confluent.io/rpm/3.1/archive.key
-enabled=1
-
-EOFrepo
-"
-    sudo yum clean all
-    sudo yum -y install confluent-platform-2.11
-}
-
 
 function confluentProps() {
 
@@ -261,7 +88,7 @@ function confluentProps() {
     
     cat > $l_tmp_script << EOFcp
 
-    cd /u01/app/oracle/product/12.3.0/ogg4bd/dirprm
+    cd ${ogg4bdHome}/dirprm
     
     cat > conflt.props << EOFcp1
 gg.handlerlist=confluent
@@ -281,11 +108,12 @@ goldengate.userexit.writers=javawriter
 javawriter.stats.display=TRUE
 javawriter.stats.full=TRUE
 gg.log=log4j
+# gg.log.level=DEBUG
 gg.log.level=INFO
 gg.report.time=30sec
 # /usr/share/java/kafka-connect-hdfs/ has everything in it
-gg.classpath=dirprm/:/u01/app/oracle/product/12.3.0/ogg4bd/ggjava/resources/lib/*:/usr/share/java/kafka/*:/u01/kafka-connect/bin/ogg-kafka-connect-1.0.jar:/usr/share/java/kafka-connect-hdfs/*
-javawriter.bootoptions=-Xmx512m -Xms32m -Djava.class.path=.:./ggjava/ggjava.jar:./dirprm
+gg.classpath=dirprm/:${ogg4bdHome}/ogg4bd/ggjava/resources/lib/*:/usr/share/java/kafka/*:/u01/kafka-connect/bin/ogg-kafka-connect-1.0.jar:/usr/share/java/kafka-connect-hdfs/*
+javawriter.bootoptions=-Xmx2048m -Xms512m -Djava.class.path=.:./ggjava/ggjava.jar:./dirprm
 EOFcp1
 
 EOFcp
@@ -303,15 +131,14 @@ function confluentProperties() {
     
     cat > $l_tmp_script << EOFcprop
 
-cd /u01/app/oracle/product/12.3.0/ogg4bd/dirprm
+cd ${ogg4bdHome}/dirprm
 
 cat > confluent.properties << EOFcprop1
 bootstrap.servers=${bootstrapServers}
-# value.serializer=org.apache.kafka.common.serialization.ByteArraySerializer
-# key.serializer=org.apache.kafka.common.serialization.ByteArraySerializer
-value.serializer=io.confluent.kafka.serializers.KafkaAvroSerializer
-key.serializer=io.confluent.kafka.serializers.KafkaAvroSerializer
 schema.registry.url=${schemaRegistry}
+#
+value.serializer=org.apache.kafka.common.serialization.ByteArraySerializer
+key.serializer=org.apache.kafka.common.serialization.ByteArraySerializer
 value.converter=org.apache.kafka.connect.json.JsonConverter
 key.converter=org.apache.kafka.connect.json.JsonConverter
 internal.value.converter=org.apache.kafka.connect.json.JsonConverter
@@ -331,12 +158,12 @@ function irconflt() {
     
     cat > $l_tmp_script << EOFrk
 
-    cd /u01/app/oracle/product/12.3.0/ogg4bd/dirprm
+    cd ${ogg4bdHome}/dirprm
     
     cat > irconflt.prm << EOFrk1
 SPECIALRUN
 END RUNTIME
-EXTFILE /u01/app/oracle/product/12.3.0/ogg4bd/dirdat/initld
+EXTFILE ${ogg4bdHome}/dirdat/initld
 --
 TARGETDB LIBFILE libggjava.so SET property=dirprm/conflt.props
 SOURCECHARSET AL32UTF8
@@ -345,8 +172,8 @@ GROUPTRANSOPS 10000
 MAP ${pdbName}.ade.*, TARGET bd.*;
 EOFrk1
 
-cd /u01/app/oracle/product/12.3.0/ogg4bd
-./replicat paramfile dirprm/irconflt.prm reportfile dirrpt/irconflt.rpt
+# cd ${ogg4bdHome}
+# ./replicat paramfile dirprm/irconflt.prm reportfile dirrpt/irconflt.rpt
     
 EOFrk
 
@@ -363,16 +190,16 @@ function rconflt1() {
     
     cat > $l_tmp_script << EOFrk
 
-    cd /u01/app/oracle/product/12.3.0/ogg4bd/dirprm
+    cd ${ogg4bdHome}/dirprm
 
     cat > rconflt1.prm << EOFrk1
 REPLICAT rconflt1
 -- Command to add REPLICAT
--- add replicat rconflt1, exttrail /u01/app/oracle/product/12.3.0/ogg4bd/dirdat/ss
-TARGETDB LIBFILE libggjava.so SET property=/u01/app/oracle/product/12.3.0/ogg4bd/dirprm/conf.props
+-- add replicat rconflt1, exttrail ${ogg4bdHome}/dirdat/ss
+TARGETDB LIBFILE libggjava.so SET property=${ogg4bdHome}/dirprm/conf.props
 REPORTCOUNT EVERY 1 MINUTES, RATE
 GROUPTRANSOPS 10000
-MAP ${pdbName}.ade.*, TARGET bd.*;
+MAP ${pdbName}.ade.*, TARGET *.*.*;
 EOFrk1
 
 EOFrk
@@ -392,6 +219,7 @@ function run()
     eval `grep bootstrapServers $INI_FILE`
     eval `grep schemaRegistry $INI_FILE`
     eval `grep pdbName $INI_FILE`
+    eval `grep ogg4bdHome $INI_FILE`
 
     l_str=""
     if [ -z $bootstrapServers ]; then
@@ -403,17 +231,14 @@ function run()
     if [ -z $pdbName ]; then
         l_str+="pdbName not found in $INI_FILE; "
     fi
+    if [ -z $ogg4bdHome ]; then
+        l_str+="ogg4bdHome not found in $INI_FILE; "
+    fi
     if ! [ -z $l_str ]; then
         fatalError "ogg4bd-testsuite(): $l_str"
     fi
 
     # function calls
-    #installDocker
-    # createTopics
-    # customKafkaProducer
-    # kafka.props
-    # rkafka
-    installConfluent
     confluentProps
     confluentProperties
     irconflt
